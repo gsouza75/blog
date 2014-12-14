dbcd495aa5179f1d7be902f8b80fa0c1817964f9
Gustavo Souza <gsouza75@gmail.com>
2014-12-14 00:01:20 -0800

    Cleanup.

--- public/js/main.js
+++ public/js/main.js
@@ -54,11 +54,6 @@
       };
     },
 
-    // initialize: function () {
-    //   this.listenTo(this.model, 'change', this.render);
-    //   this.listenTo(this.model, 'destroy', this.remove);
-    // },
-
     render: function () {
       this.$el.html(this.template(this.model.toJSON()));
       return this;
@@ -115,7 +110,6 @@
 
     initialize: function () {
       this.listenTo(this.model, 'change:activeIndex', this.update);
-      // this.listenTo(this.collection, 'add remove', this.render);
     },
 
     handleItemClick: function (e) {
